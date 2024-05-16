d6b5b9abd2e7c31a6a2fa86498c1245d886c9907
joecus1 <joecusano57@gmail.com>
2024-05-15 20:01:14 -0400

    response added

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,14 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+Socket socket = server.AcceptSocket(); // wait for client
+Console.Write("Connected!");
+
+var responseString = "HTTP/1.1 200 OK\r\n\r\n";
+Byte[] responseBytes = Encoding.ASCII.GetBytes(responseString);
+int i = socket.Send(responseBytes);
+Console.WriteLine($"Responded: {responseString}");
